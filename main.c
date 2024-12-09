1189310d014d7a06bf627e3695f477beddae7bff
Breit <l.breitenstein@students.ephec.be>
2024-12-09 14:56:33 +0100

    Structure du projet

--- main.c
+++ main.c
@@ -1,1 +1,25 @@
 
+#include <stdio.h>
+
+// Déclaration des fonctions prototypes
+void afficherNomPilote();
+void afficherStatsCourse();
+
+int main() {
+    printf("Bienvenue dans le projet Formule 1 !\n");
+    
+    // Appel des fonctions exemple
+    afficherNomPilote();
+    afficherStatsCourse();
+
+    return 0;
+}
+
+// Définition des fonctions
+void afficherNomPilote() {
+    printf("Nom du pilote: Lewis Hamilton\n");
+}
+
+void afficherStatsCourse() {
+    printf("Statistiques de la course: 10 victoires, 5 pole positions\n");
+}
